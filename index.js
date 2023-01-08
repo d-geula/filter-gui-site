98eeba7b05c82df5a2466845ca625b2e9e831e6b
Daniel Geula <d.geula@gmail.com>
2023-01-08 08:10:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const outputField = document.getElementById("output-field");
+const addRule = document.getElementById("add-rule");
+const resetRules = document.getElementById("reset-rules");
+const exportRules = document.getElementById("export-rules");
+
 // List of element IDs containing the data fields
 const dataFields = [
     "character",
@@ -9,20 +14,15 @@ const dataFields = [
     "minBlessingRarity",
     "minPerkRarity",
     "shop"
-    ];
+];
 
-const outputField = document.getElementById("output-field");
-const addRule = document.getElementById("add-rule");
-const resetRules = document.getElementById("reset-rules");
-const exportRules = document.getElementById("export-rules");
-
-// Create list to store the data for all entries
 var rules = [];
 
 // Button functions
 function add_rules() {
     var rule = {};
-    // Loop through all elements and get the values from the input fields
+
+    // Loop through id's in dataFields list to get the values from the input fields
     // If the value is not empty, add it to the rule object
     for (var i = 0; i < dataFields.length; i++) {
         if (document.getElementById(dataFields[i]).value != "") {
@@ -34,12 +34,15 @@ function add_rules() {
         }
         }
     }
+
     // Add the object to the rules list
     rules.push(rule);
+
     // Clear the input fields
     for (var i = 0; i < dataFields.length; i++) {
         document.getElementById(dataFields[i]).value = "";
     }
+    
     // Add the content of the rules list to the output field
     outputField.innerHTML = JSON.stringify(rules, null, 4);
 }
@@ -63,26 +66,13 @@ function export_rules() {
         alert("You must add at least one rule before you can export");
         return;
     }
+    
     // Get contents of the text widget
     var text = outputField.innerHTML;
-    // Create a new blob with the contents of the text widget
+    // Create a new blob with the contents of the output field
     var blob = new Blob([text], {type: "text/plain"});
-    // Create a new URL object with the blob
-    var url = URL.createObjectURL(blob);
-    // Create a new link element
-    var link = document.createElement("a");
-    // Set the link's href to the URL object
-    link.setAttribute("href", url);
-    // Set the link's download attribute
-    link.setAttribute("download", "filter_rules.txt");
-    // Append the link to the body
-    document.body.appendChild(link);
-    // Click the link
-    link.click();
-    // Remove the link from the body
-    document.body.removeChild(link);
-    // Revoke the URL object
-    URL.revokeObjectURL(url);
+    // Save the Blob object as a local file using FileSaver.js
+    saveAs(blob, "filter_rules.txt");
 }
 
 // Event listeners
